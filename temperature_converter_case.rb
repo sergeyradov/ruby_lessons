abc21220d2f84a0886bcb585444959e5bfb1e8db
Sergey.Radov <sergey.radov@opower.com>
2017-04-16 04:47:37 +0300

    applied rubocop recommendations wq2.1

--- temperature_converter_case.rb
+++ temperature_converter_case.rb
@@ -1,37 +1,35 @@
 
 
-warningMessage="Please double check the number that you've entered."
-celciusMessage="I have detected temperature in Celcius."
-errorMessage="It looks like you have enter data incorrectly!"
-hotMessage="ohh..It's hot."
-coldMessage="wow..it's cold."
+error_message = 'It looks like you have enter data incorrectly!'
+error_miissed_symbol = 'Hoo.. have you entered C or F'
+error_wrong_symbols = 'Hmm... It looks I can\'t convert from this degree.'
 
-print "Please enter temperature in Celcius or Fahrenheit : "
+print 'Please enter temperature in Celcius or Fahrenheit : '
 
-data=gets.chomp!
- 
-if data.nil? or data.strip.empty?
-	puts errorMessage
-	return
+data = gets.chomp!
+
+if data.nil? || data.strip.empty?
+  puts error_message
+  return
 end
 
-value,sign = data.split(' ')
+value, sign = data.split(' ')
 
- unless sign.nil? or value.nil? or sign.empty? or value.empty?
+if sign.nil? || value.nil?
+  puts error_miissed_symbol
+  return
+end
 
-     case sign.upcase
-     when 'C'	    
+unless sign.nil? || value.nil? || sign.empty? || value.empty?
 
-       fahrenheitTemp = value.to_f * 1.8 + 32
-       puts "This temperature in Fahrenheit : #{fahrenheitTemp} F"
-     
-     when 'F'    
-
-       celciusTemp = ((value.to_f - 32) / 1.8).round(2)
-       puts "This temperature in Celcius : #{celciusTemp} C"
-     else 
-	puts errorMessage
-     end
- else 
-  puts errorMessage	 
- end
+  case sign.upcase
+  when 'C'
+    fahrenheit_temp = value.to_f * 1.8 + 32
+    puts "This temperature in Fahrenheit : #{fahrenheit_temp} F"
+  when 'F'
+    celcius_temp = ((value.to_f - 32) / 1.8).round(2)
+    puts "This temperature in Celcius : #{celcius_temp} C"
+  else
+    puts error_wrong_symbols
+  end
+end
