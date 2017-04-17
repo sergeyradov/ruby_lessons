ccb883607f37ea7ecb9ab8330150c56b34073314
Sergey.Radov <sergey.radov@opower.com>
2017-04-18 00:13:15 +0300

    Added usage of two first args only. others are ignored

--- temperature_converter_case_args.rb
+++ temperature_converter_case_args.rb
@@ -1,10 +1,8 @@
 
-
 error_miissed_symbol = 'Hoo.. have you entered C or F'
 error_wrong_symbols = 'Hmm... It looks I can\'t convert from this degree.'
 
-value = ARGV[0]
-sign = ARGV[1]
+value,sign,*rest_args = ARGV
 
 if sign.nil? || value.nil?
   puts error_miissed_symbol
@@ -16,7 +14,7 @@ unless sign.nil? || value.nil? || sign.empty? || value.empty?
   case sign.upcase
   when 'C'
     fahrenheit_temp = value.to_f * 1.8 + 32
-    puts "This temperature in Fahrenheit : #{fahrenheit_temp} F"
+    puts "This temperature in Fahrenheit : #{fahrenheit_temp} F" 
   when 'F'
     celcius_temp = ((value.to_f - 32) / 1.8).round(2)
     puts "This temperature in Celcius : #{celcius_temp} C"
