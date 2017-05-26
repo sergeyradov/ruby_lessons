34fadc4f78db60b921662023955853b97150daf2
Sergey.Radov <sergey.radov@opower.com>
2017-05-26 12:44:23 +0300

    added Const get

--- inheritance/area_calculator.rb
+++ inheritance/area_calculator.rb
@@ -2,7 +2,7 @@
 # Plan Do it please! Lesson 9 algthorithm 
 # TZ 
 # I/o
-# Algo
+# check ./PseudoCode_AreaCalculator.txt
 # UML
 # Coding
 # RTesting
@@ -10,41 +10,92 @@
 #Figures : Rectagle,Circle,Treangle   
 
 class Figure 
- attr_accessor:color, :shape	
-	def area
-    		print self.class.name
- 	end
+ 	attr_accessor :shape
+
+	def initialize (shape)
+	        @share = shape
+	end
 end 
 
-class Treangle < Figure
+class Triangle < Figure
 
-       def area(a=1,b=1,c=1)	
-		puts self.class.name
-		#S = √p(p - a)(p - b)(p - c)
-		 p = (a + b + c)*0.5
-		return Math.sqrt(p*(p-a)*(p-b)*(p-c));
+	attr_accessor :side_a, :side_b, :side_c
+
+        def initialize (shape,side_a,side_b,side_c)
+             super(shape)
+   	     @side_a = side_a.to_f
+	     @side_b = side_b.to_f
+	     @side_c = side_c.to_f
+	end
+	
+	def area
+		p = ( @side_a + @side_b + @side_c)/2
+		return Math.sqrt(p*(p-@side_a)*(p-@side_b)*(p-@side_c));
        end
 end
 
-class Circle < Figure 
-	def area(r=1)
-		puts self.class.name
-		return Math::PI*r*r
+class Circle < Figure
+
+	attr_accessor :radius
+
+	def initialize(shape,radius)
+		super(shape)
+		@radius = radius.to_f
+	end
+
+	def area
+		return Math::PI*@radius*@radius
 	end
 end 
 
 class Rectangle < Figure
-	def area(a=1,b=2)
-		puts self.class.name
-		return a*b
+
+	attr_accessor :side_a, :side_b
+
+	def initialize (shape,side_a,side_b)
+		super(shape)
+	     	@side_a = side_a.to_f
+		@side_b = side_b.to_f
+	end
+	def area
+#		puts self.class.name
+		return side_a * side_b
 	end
 end
 
 
-file = File.new("figures.txt", "r")
-while (line = file.gets)
-	    puts "#{line}"
+
+class FigureReader
+	
+	attr_accessor :file
+
+	def initialize (file)
+	  @file = File.new(file, "r")	
+        end
+
+	def calculate_area 
+		while (line = @file.gets)
+	
+			data = line.chomp!.split(',')
+  			
+			figure, *the_rest = data
+
+			figure.capitalize!
+
+			currentFigure = Object.const_get(figure).new(figure, *the_rest)
+
+		        puts currentFigure.class.name + " : area " + currentFigure.area.round(1).to_s
+			
+			currentFigure = nil
+		end
+
+		file.close
+	end
 end
-file.close
+
+
+
+
+FigureReader.new('figures.txt').calculate_area
 
 
